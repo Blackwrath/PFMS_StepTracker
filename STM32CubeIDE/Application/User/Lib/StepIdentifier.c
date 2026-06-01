b1159e732015334be0d21aadc23994a43848725c
PotatoPrism <57695937+Blackwrath@users.noreply.github.com>
2026-06-01 23:45:33 +1000

    Create StepIdentifier.c

---
+++ STM32CubeIDE/Application/User/Lib/StepIdentifier.c
@@ -0,0 +1,57 @@
+/*
+ * StepIdentifier.c
+ *
+ *  Created on: May 4, 2026
+ *      Author: willa
+ */
+struct Vector3 { // Imma need to do some vector maths, so I'm making my own collection of functions for working with Vector3
+	int x;
+	int y;
+	int z;
+}
+
+Vector3 vecSub(Vector3 v1, Vector3 v2){
+	Vector3 out;
+	out.x = v1.x-v2.x;
+	out.y = v1.y-v2.y;
+	out.z = v1.z-v2.z;
+	return out;
+}
+Vector3 scalarMult(int s, Vector3 v1){
+	Vector3 out;
+	out.x = v1.x*s;
+	out.y = v1.y*s;
+	out.z = v1.z*s;
+	return out;
+}
+int dotProd(Vector3 v1, Vector3 v2){
+	int out = v1.x*v2.x + v1.y-v2.y + v1.z*v2.z;
+	return out;
+}
+int magSquared(Vector3 v1){
+	int out = v1.x*v1.x+v1.y*v1.y+v1.z*v1.z;
+	return out;
+}
+
+
+
+void TrackStep(Vector3 currentVector, Vector3 calibrationVector, bool* Step,int* stepCount){
+	int threshhold = 100;
+	// Assume the calibration vector is the direction gravity is pointing.
+	int calibrationMag = magSquared(calibrationVector);
+	Vector3 CurrentVectorCalibrated = vecSub(currentVector,calibrationVector); // This is the current acceleration taking into account gravity
+	int dir = dotProd(CurrentVectorCalibrated,calibrationVector); // How much of the current vector is pointing along the calibrated gravity vector?
+	Vector3 MagnitudeVector = scalarMult(dir,calibrationVector); // Projection of the calibrated vector onto the calibration vector
+	int finalMag = magSquared(MagnitudeVector);
+	if (finalMag>calibrationMag*threshhold*threshhold){// If we're above the threshhold
+		if(!*Step){ // If we're not already in a step
+			*stepCount +=1; //increment the step count
+			*Step = true; //We are now in a step
+		}// If we're above the threshhold, but currently in a step do nothing
+	}else{// If we're below the threshhold
+		if(*Step){
+			*Step = false; //We are now below the threshhold, so we are no longer in a step
+		}
+	}
+
+}
